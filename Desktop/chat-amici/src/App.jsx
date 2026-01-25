8e0978404bbc10b920b4fc7101d8731486800e6f
graziano <grazianobares@gmail.com>
2026-01-25 10:53:47 +0100

    debug env

--- Desktop/chat-amici/src/App.jsx
+++ Desktop/chat-amici/src/App.jsx
@@ -7,6 +7,7 @@ const supabase = createClient(
 );
 
 const TABLE = "securemov";
+console.log("SUPABASE_URL:", import.meta.env.VITE_SUPABASE_URL);
 
 export default function App() {
   const [nome, setNome] = useState(localStorage.getItem("nome_utente") || "");
